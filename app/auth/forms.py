8c4a787d011573bfced022e0b6f1120502c71a8e
Nyash-Mauro <mauricee423@gmail.com>
2020-10-25 02:54:45 +0300

    affef a function to login the user

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -17,3 +17,9 @@ class Reistration(FlaskForm):
     def validate_username(self,data_field):
         if User.query.filter_by(username =data_field.data).first():
             raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[DataRequired(),Email()])
+    password = StringField('Password',validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('SignIn')
