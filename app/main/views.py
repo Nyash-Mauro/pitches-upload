c36b8bf4f3a3b912cf18cbe42aa3f0e5eab36c56
Nyash-Mauro <mauricee423@gmail.com>
2020-10-25 18:24:36 +0300

    added function to update pic profile

--- app/main/views.py
+++ app/main/views.py
@@ -48,3 +48,14 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
     title = 'Update || Profile'
     return render_template('update_profile.html',form=form,title=title)
+
+@main.route('/user/<uname>/upadate/pic',methods=['POST'])
+@login_required()
+def update_pic(uname):
+    user = User.query.filter_by(username=uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
