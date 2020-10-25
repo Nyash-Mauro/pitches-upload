bacc8027b369aca707de68ac95652ff23a9f296e
Nyash-Mauro <mauricee423@gmail.com>
2020-10-25 04:08:18 +0300

    added func to update userprofile

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,7 @@ class CommentsForms(FlaskForm):
     comment= TextAreaField('Comment',validators=DataRequired())
     vote=RadioField('default arguments',choices=[('1','UpVote'),('1''DownVote')])
     submit=SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us more about you',validators=DataRequired())
+    submit = SubmitField('Submit')
