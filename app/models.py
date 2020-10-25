e4360a1e6a591d5201880f8b67aaf40af7f9a60a
Nyash-Mauro <mauricee423@gmail.com>
2020-10-25 22:32:10 +0300

    function to get all the pitches in database

--- app/models.py
+++ app/models.py
@@ -32,3 +32,30 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User{self.username}'
+
+class Pitch(db.Model):
+    '''
+    Pitch class to define Pitch Objects
+    '''
+    __tablename__ = 'pitch'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch = db.Column(db.String)
+    category_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comments = db.relationship('Comment',backref = 'pitch',lazy="dynamic")
+        
+
+    def save_pitch(self):
+        '''
+        Function that saves pitches
+        '''
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def get_all_pitches(cls):
+        '''
+        Function that queries the databse and returns all the pitches
+        '''
+        return Pitch.query.all()
