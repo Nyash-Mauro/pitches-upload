62d53ca4eac5ff983230409113bd1d69505c3a76
Nyash-Mauro <mauricee423@gmail.com>
2020-10-25 16:02:04 +0300

    added function to check interview pitches

--- app/main/views.py
+++ app/main/views.py
@@ -9,8 +9,15 @@ from .. import db,photos
 def index():
     """ View root page func that returns the index page and its data """
     return render_template('index.html')
-render_template
+
 @main.route('/all_pitches')
 def all_pitches():
     general = Pitch.query.all()
     return render_template('all pitches.html',general=general)
+
+@main.route('/interview')
+def interview():
+    comment =Comment.query.all()
+    interview = Pitch.query.filter_by(category ='Interview Pitch').all()
+    return render_template('interview.html',interview=interview,comment=comment)
+
