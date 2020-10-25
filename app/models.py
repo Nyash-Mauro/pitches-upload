242887fd1d51cd13011ba05cdc87cd1a9e42e745
Nyash-Mauro <mauricee423@gmail.com>
2020-10-25 22:35:18 +0300

    function to get all the database comments

--- app/models.py
+++ app/models.py
@@ -59,3 +59,39 @@ class Pitch(db.Model):
         Function that queries the databse and returns all the pitches
         '''
         return Pitch.query.all()
+
+    @classmethod
+    def get_pitches_by_category(cls,cat_id):
+        '''
+        Function that queries the databse and returns pitches based on the
+        category passed to it
+        '''
+        return Pitch.query.filter_by(category_id= cat_id)
+# function to get comments in db
+class Comment(db.Model):
+
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment= db.Column(db.String)
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitch.id'))
+    username =  db.Column(db.String)
+    votes= db.Column(db.Integer)
+    
+
+    def save_comment(self):
+        '''
+        Function that saves comments
+        '''
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def clear_comments(cls):
+        Comment.all_comments.clear()
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+
+        return comments
